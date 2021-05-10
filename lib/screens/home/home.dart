9e440a2ca81522dd2a7630f59712c30d6aabffa5
izaanjahangir <izaanjahangir2@gmail.com>
2021-05-11 02:32:45 +0500

    improvements

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -12,16 +12,32 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final double drawerWidth = size.width;
 
     return SafeArea(
       child: Scaffold(
         drawer: Responsive.isDesktop(context)
             ? null
-            : Drawer(
-                child: AppDrawer(),
+            : Container(
+                width: drawerWidth > 250 ? 250 : drawerWidth,
+                child: Drawer(
+                  child: AppDrawer(),
+                ),
               ),
-        body: SingleChildScrollView(
-          child: Container(width: size.width, child: Text("Home")),
+        body: Container(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (Responsive.isDesktop(context))
+                Container(
+                  width: drawerWidth > 250 ? 250 : drawerWidth,
+                  child: AppDrawer(),
+                ),
+              Container(
+                child: Text("Home"),
+              )
+            ],
+          ),
         ),
       ),
     );
