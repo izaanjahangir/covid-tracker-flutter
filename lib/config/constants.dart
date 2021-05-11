be6bfb875e89fb8ba022b7cb08eeddb3eda987b9
izaanjahangir <izaanjahangir2@gmail.com>
2021-05-12 03:43:28 +0500

    added list of supported countries

---
+++ lib/config/constants.dart
@@ -0,0 +1,20 @@
+import 'package:covid_tracker/models/country.dart';
+
+class Constants {
+  static final flag = [
+    Country(name: "Pakistan", code: "PAK", flag: "assets/icons/pakistan.svg"),
+    Country(name: "United Kingdom", code: "GBR", flag: "assets/icons/uk.svg"),
+    Country(name: "United States", code: "USA", flag: "assets/icons/us.svg"),
+    Country(name: "India", code: "IND", flag: "assets/icons/india.svg"),
+    Country(name: "China", code: "CHN", flag: "assets/icons/china.svg"),
+    Country(name: "Canada", code: "CAN", flag: "assets/icons/canada.svg"),
+    Country(name: "France", code: "FRA", flag: "assets/icons/france.svg"),
+    Country(name: "Spain", code: "ESP", flag: "assets/icons/spain.svg"),
+    Country(name: "Italy", code: "ITA", flag: "assets/icons/italy.svg"),
+    Country(name: "Iran", code: "IRI", flag: "assets/icons/iran.svg"),
+    Country(name: "Turkey", code: "TUR", flag: "assets/icons/turkey.svg"),
+    Country(name: "Germany", code: "GHA", flag: "assets/icons/germany.svg"),
+    Country(name: "Mexico", code: "MEX", flag: "assets/icons/mexico.svg"),
+    Country(name: "Greece", code: "GRL", flag: "assets/icons/greece.svg")
+  ];
+}
---
+++ lib/models/country.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Country {
+  final String name;
+  final String flag;
+  final String code;
+
+  Country({@required this.name, @required this.flag, @required this.code});
+}
