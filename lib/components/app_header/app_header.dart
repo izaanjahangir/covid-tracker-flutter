0c8ea468def0cdf393458856cf76c109676052a1
izaanjahangir <izaanjahangir2@gmail.com>
2021-05-11 23:39:01 +0500

    improvements

--- lib/components/app_header/app_header.dart
+++ lib/components/app_header/app_header.dart
@@ -1,3 +1,4 @@
+import 'package:covid_tracker/utils/responsive.dart';
 import "package:flutter/material.dart";
 import "package:covid_tracker/config/theme_colors.dart";
 import 'package:touchable_opacity/touchable_opacity.dart';
@@ -5,6 +6,8 @@ import 'package:touchable_opacity/touchable_opacity.dart';
 class AppHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bool isNotDesktop = !Responsive.isDesktop(context);
+
     void openDrawer() {
       Scaffold.of(context).openDrawer();
     }
@@ -16,17 +19,19 @@ class AppHeader extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Row(
         children: [
-          TouchableOpacity(
-            onTap: openDrawer,
-            child: Icon(
-              Icons.menu,
-              size: 25,
-              color: ThemeColors.white,
+          if (isNotDesktop)
+            TouchableOpacity(
+              onTap: openDrawer,
+              child: Icon(
+                Icons.menu,
+                size: 25,
+                color: ThemeColors.white,
+              ),
             ),
-          ),
-          SizedBox(
-            width: 10,
-          ),
+          if (isNotDesktop)
+            SizedBox(
+              width: 10,
+            ),
           Text(
             "Pakistan (10000)",
             style: TextStyle(fontSize: 18, color: ThemeColors.white),
